dcec76dbb09d9c5029fe3c7116613cb53d88a218
= <=>
2022-05-05 12:41:57 -0300

    backend

---
+++ server/src/server.ts
@@ -0,0 +1,46 @@
+import express from 'express'
+import nodemailer from 'nodemailer'
+import { prisma } from './prisma'
+
+const app = express()
+
+app.use(express.json())
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '960165baece06b',
+    pass: '1dadfbf510b554'
+  }
+})
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'jean.carlos.ti@gmail.com',
+    subject: 'Novo feedback',
+    html: [
+      `<div styl;e="font-famaly: sans-serif; font-size: 16px; color: #111">`,
+      `<p> Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
+  })
+
+  return res.status(201).json({ data: feedback })
+})
+
+app.listen(3333, () => {
+  console.log('HTTP server running!')
+})
